aada0c2dbedbec32ef9a5ae3edfc4de9a7ee7a4f
Paige Williams <paw145@humboldt.edu>
2018-11-12 20:40:52 -0800

    Add multiple percentage functions.

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+//Back-end logic
+var calcPercent = function (number, percent) {
+  var product = number * percent;
+  var quotient = parseFloat(product / 100);
+  return quotient
+}
+
+var calcPercent2 = function (number, number1) {
+  var quotient = number / number1;
+  var product = parseFloat(quotient * 100);
+  return product
+}
+
+var calcPercent3 = function (number, percent) {
+  convertDecimal = parseFloat(percent / 100)
+  quotient = parseFloat(number / convertDecimal)
+  return quotient
+}
+
+var percentIncrease = function (number, number1){
+  percentIncrease = parseInt(((number1 - number)/ number) * 100)
+  return percentIncrease
+}
+
+var percentDecrease = function (number, number1){
+  percentDecrease = parseInt(((number - number1)/ number) * 100)
+  return percentDecrease
+}
